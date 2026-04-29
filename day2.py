d3b32a6bbcddd8f80966386ec4f4aaf4ae6025fc
Misab Varikkappulakkal <misabvarikkappulakkal@gmail.com>
2026-04-29 09:50:52 +0530

    Day 2: Learned variables in Python

---
+++ day2.py
@@ -0,0 +1,3 @@
+name = "john"
+print(f"Hello, {name}!")
+# This is a simple Python program that prints a greeting message to the console. The variable 'name' is assigned the value "john", and then the print function is used to display "Hello, john!" using an f-string for formatting.
