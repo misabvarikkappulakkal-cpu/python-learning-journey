eebd1483d1452ec66f49d4bff888c032276d6f0a
Misab Varikkappulakkal <misabvarikkappulakkal@gmail.com>
2026-05-01 16:40:54 +0530

    Day 4: Learned loops (for and while) in Python

---
+++ basics/day4.py
@@ -0,0 +1,12 @@
+#For loop
+for i in range(1, 11):
+    if i % 2 == 0:
+        print(f"{i} is even")
+    else:
+        print(f"{i} is odd")
+
+#While loop
+i = 2
+while i <= 10:
+    print(i)
+    i += 2
